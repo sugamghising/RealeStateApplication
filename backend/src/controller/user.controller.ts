d5231597425df85d4f56f5ee00f551fa748d5a87
Sugam Ghising <ghisingsugam04@gmail.com>
2025-12-15 13:33:18 +0545

    feat: implement user controller and service with basic CRUD structure

---
+++ backend/src/controller/user.controller.ts
@@ -0,0 +1,17 @@
+import { Request, Response } from "express";
+
+export const getUsers = async (req: Request, res: Response) => {
+
+}
+
+export const getUser = async (req: Request, res: Response) => {
+
+}
+
+export const updateUser = async (req: Request, res: Response) => {
+
+}
+
+export const deleteUser = async (req: Request, res: Response) => {
+
+}
---
+++ backend/src/services/user.service.ts
@@ -0,0 +1,24 @@
+import { prisma } from "../lib/prisma"
+
+export const getUsers = async () => {
+    try {
+        const users = await prisma.user.findMany();
+        return users;
+    } catch (error) {
+        console.error("Error fetching users.", error);
+    }
+}
+
+export const getUserById = async (userId: string) => {
+    const user = await prisma.user.findUnique({
+        where: { id: userId },
+    });
+    if (!user) {
+        throw new Error("User not found.");
+    }
+    return user;
+}
+
+// export const updateUser = async (userData:) => {
+
+// }
